13151e26f21edb8b0e2db49c5f45019b1a2b584e
Ritik Jain <ritik9628@gmail.com>
2025-09-22 22:35:47 +0530

    minor fix

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const port = 30000;
-// const baseURL = `http://a7eab011cae114711ada76d353f964c3-1474535787.us-west-2.elb.amazonaws.com`;
+const baseURL = `http://a7eab011cae114711ada76d353f964c3-1474535787.us-west-2.elb.amazonaws.com`;
 // const baseURL = `http://mern-backend-service.mern-app.svc.cluster.local:5000`;
-const baseURL = import.meta.env.VITE_BACKEND_URL;
+// const baseURL = import.meta.env.VITE_BACKEND_URL;
 // const baseURL = `${window.location.protocol}//${window.location.hostname}:${port}`;
 
 const myAxios = axios.create({
