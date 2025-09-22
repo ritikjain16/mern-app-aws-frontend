e94cef25c51d37204a1eba3b40f54d02ab74b310
Ritik Jain <ritik9628@gmail.com>
2025-09-22 21:32:09 +0530

    changed service to loadbalancer

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const port = 30000;
-const baseURL = `${window.location.protocol}//${window.location.hostname}:${port}`;
+const baseURL = `http://a7eab011cae114711ada76d353f964c3-1474535787.us-west-2.elb.amazonaws.com`;
+// const baseURL = `${window.location.protocol}//${window.location.hostname}:${port}`;
 
 const myAxios = axios.create({
   baseURL,
