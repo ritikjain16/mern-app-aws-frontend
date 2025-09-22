02a12457aede850899d867529c033db35fe2c078
Ritik Jain <ritik9628@gmail.com>
2025-09-22 22:40:53 +0530

    backend url changed

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const port = 30000;
-const baseURL = `http://a7eab011cae114711ada76d353f964c3-1474535787.us-west-2.elb.amazonaws.com`;
+const baseURL = `a7eab011cae114711ada76d353f964c3-1335311020.us-west-2.elb.amazonaws.com`;
 // const baseURL = `http://mern-backend-service.mern-app.svc.cluster.local:5000`;
 // const baseURL = import.meta.env.VITE_BACKEND_URL;
 // const baseURL = `${window.location.protocol}//${window.location.hostname}:${port}`;
